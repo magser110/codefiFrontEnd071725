0af9258c5489474bada57626afd4fdb6c7d6e7b4
magser110 <magaly.serrano01@yahoo.com>
2025-08-11 20:07:24 -0500

    saving for vercel

--- fe_books/src/app/navbar/navbar.component.ts
+++ fe_books/src/app/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   constructor(public authService: AuthenticationService) {}
-  
+
   logout(){
     this.authService.logout();
   }
